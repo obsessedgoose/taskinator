d67a8aa3822959a3451d46f3eb103ffcfbcc5080
Kyle <68563225+obsessedgoose@users.noreply.github.com>
2020-08-19 16:29:57 -0600

    Add files via upload
    added function to type input and drop down type to be displayed when clicked. added style to input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,50 @@
-var formEl = document.querySelector("#task-form");
+window.document.querySelector("button");
+var formEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var createTaskEl = function(taskDataObj) {
+    name = "Task's name",
+    type = "Task's type"
 
-formEl.addEventListener("submit", createTaskHandler);
+    // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
 
-var createTaskHandler = function(event) {
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("click", function (event) {
 
     event.preventDefault();
 
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-};
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var tasksToDoEl = document.querySelector("#tasks-to-do");
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+
+
+});
 
